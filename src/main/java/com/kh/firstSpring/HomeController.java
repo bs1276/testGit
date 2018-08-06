6124126799d9f829241295c59a590984ac568f32
user2 <user2@user2-PC>
2018-08-06 17:40:41 +0900

    test

--- src/main/java/com/kh/firstSpring/HomeController.java
+++ src/main/java/com/kh/firstSpring/HomeController.java
@@ -19,11 +19,10 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
-	 * Simply selects the home viewswerwerdfsdf to render by returning its name.
 	 */
 	@RequestMapping(value = "index.do", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		//test
+		//testetwetrw
 		return "index";
 	}
 }
